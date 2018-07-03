22ede7c31f24f1772e599f73ca3256f985ba33ec
Alejandro Rico <alejandro.rico@gmail.com>
2018-07-03 15:27:50 -0300

    "Last commit, I won't be able to play much, so I can't test it."

--- Layout.lua
+++ Layout.lua
@@ -17,26 +17,43 @@ local ICON_SIZE = 20
 local config = {
 	player = {
 		width = 250,
-		height = 41,
+		height = 40,
 		power = 5,
+		aura = {
+			b = 20,
+			d = 27
+		}
 	},
 	target = {
 		width = 250,
-		height = 41,
+		height = 40,
 		health = 35,
 		power = 5,
+		aura = {
+			b = 20,
+			d = 27
+		}
 	},
 	targettarget = {
 		width = 150,
-		height = 31,
+		height = 30,
 		health = 20,
 		power = 10,
 	},
 	focus = {
 		width = 150,
-		height = 31,
+		height = 30,
 		health = 20,
 		power = 10,
+		aura = {
+			b = 20,
+			d = 27
+		}
+	},
+	boss = {
+		width = 200,
+		height = 40,
+		power = 5
 	}
 }
 
@@ -104,14 +121,14 @@ local UnitSpecific = {
 		-- Power
 		self.Power:SetWidth(config.player.width)
 		self.Power:SetHeight(config.player.power)
-		self.Power:SetPoint("TOPLEFT", self, "TOPRIGHT", 10, -6)
+		self.Power:SetPoint("TOPLEFT", self, "TOPRIGHT", 10, 0)
 
 		-- Additional Power
 		if hasThirdBar[playerClass] then
 			local AdditionalPower = CreateFrame("StatusBar", nil, self)
 			AdditionalPower:SetWidth(config.player.width)
 			AdditionalPower:SetHeight(config.player.power)
-			AdditionalPower:SetPoint("TOPLEFT", self, "TOPRIGHT", 10, 0)
+			AdditionalPower:SetPoint("TOP", self.Power, "BOTTOM", 0, -1)
 			AdditionalPower:SetBackdrop(EDGE)
 			AdditionalPower:SetStatusBarTexture(TEXTURE)
 			AdditionalPower:SetBackdropColor(0, 0, 0)
@@ -129,7 +146,7 @@ local UnitSpecific = {
 				PainValue:SetTextColor(1, 1, 1)
 				self.PainValue = PainValue
 				self:Tag(self.PainValue, "[ignorepain:cur]/[ignorepain:max]")
-				self.PainValue:SetPoint("BOTTOM", self.IgnorePain, 0, 1)
+				self.PainValue:SetPoint("TOP", self.IgnorePain, 0, -1)
 			end
 		end
 
@@ -145,8 +162,8 @@ local UnitSpecific = {
 		-- Debuffs
 		local Debuffs = CreateFrame("Frame", nil, self)
 		Debuffs:SetWidth(config.player.width)
-		Debuffs:SetHeight(ICON_SIZE * 2)
-		Debuffs.size = ICON_SIZE
+		Debuffs:SetHeight(config.player.aura.d * 2)
+		Debuffs.size = config.player.aura.d
 		Debuffs.spacing = 2
 		Debuffs["growth-y"] = "UP"
 		Debuffs.initialAnchor = "BOTTOMLEFT"
@@ -157,7 +174,7 @@ local UnitSpecific = {
 		self.HealthText:SetPoint("RIGHT", self, "RIGHT", -1, 0)
 
 		-- Power Tags position and settings
-		self.PowerValue:SetPoint("CENTER", self.Power, "CENTER", 0, -1)
+		self.PowerValue:SetPoint("BOTTOM", self.Power, "BOTTOM", 0, 1)
 
 		-- Level & Name Tag
 		self.NameText:SetPoint("LEFT", self, "LEFT", 1, 0)
@@ -165,12 +182,12 @@ local UnitSpecific = {
 
 		-- Castbar
 		self.CastbarFrame:SetWidth(config.player.width)
-		self.CastbarFrame:SetHeight(ICON_SIZE)
+		self.CastbarFrame:SetHeight(20)
 		self.CastbarFrame:SetPoint("TOPLEFT", self, "BOTTOMLEFT", 0, -3)
 
 		local SpellIcon = self.Castbar:CreateTexture(nil, "OVERLAY")
 		SpellIcon:SetPoint("TOPLEFT", self.CastbarFrame)
-		SpellIcon:SetSize(ICON_SIZE, ICON_SIZE)
+		SpellIcon:SetSize(20, 20)
 		SpellIcon:SetTexCoord(.06, .94, .06, .94)
 
 		local TimeText = self.Castbar:CreateFontString(nil, "OVERLAY", "TextNormalRight")
@@ -186,25 +203,47 @@ local UnitSpecific = {
 
 		-- Gotta think something better for this.
 		if(playerClass == "ROGUE" or playerClass == "DRUID" or playerClass == "MONK") then
-			local ClassIcons = {}
-			local colors = self.colors.class[playerClass]
-			for index = 1, 6 do
-				--local Icon = self:CreateTexture(nil, "BACKGROUND")
-				-- Position and size.
-				--Icon:SetSize(16, 16)
-				local Icon = CreateFrame("Frame", nil, self)
-				Icon:SetSize(10, 20)
-				local Bar = Icon:CreateTexture(nil, "ARTWORK")
-				Bar:SetTexture(TEXTURE)
-				Bar:SetVertexColor(colors.r, colors.g, colors.b)
-				Icon:SetBackdrop(EDGE)
-				Icon:SetBackdropBorderColor(0, 0, 0)
-				Icon:SetPoint("CENTER", UIParent, "CENTER", index * Icon:GetWidth() + 1, -200)
-				ClassIcons[index] = Icon
+			local ClassPower = CreateFrame("Frame", nil, self)
+			ClassPower:SetSize(245, config.player.power)
+			ClassPower:SetPoint("TOP", self.Power, "BOTTOM", 0, -1)
+			comboWidth = (245 / MAX_COMBO_POINTS) - 1
+			for index = 1, 10 do
+				ClassPower[index] = CreateFrame('StatusBar', nil, ClassPower)
+				ClassPower[index]:SetSize(comboWidth, config.player.power)
+				ClassPower[index]:SetStatusBarTexture(TEXTURE)
+				ClassPower[index]:SetBackdrop(EDGE)
+				ClassPower[index]:SetBackdropBorderColor(0, 0, 0)
+				if index == 1 then
+					ClassPower[index]:SetPoint("TOPLEFT", ClassPower)
+				else
+					ClassPower[index]:SetPoint("LEFT", ClassPower[index - 1], "RIGHT", 1, 0)
+				end
 			end
-			self.ClassIcons = ClassIcons
+			self.ClassPower = ClassPower
 		end
 
+		-- Runes
+		if(playerClass == "DEATHKNIGHT") then
+			local Runes = CreateFrame("Frame", nil, self)
+			Runes:SetSize(245, config.player.power)
+			Runes:SetPoint("TOP", self.Power, "BOTTOM", 0, -1)
+			comboWidth = (245 / 6) - 1
+			for index = 1, 6 do
+				-- Position & size
+				Runes[index] = CreateFrame("StatusBar", nil, Runes)
+				Runes[index]:SetSize(comboWidth, config.player.power)
+				Runes[index]:SetStatusBarTexture(TEXTURE)
+				Runes[index]:SetBackdrop(EDGE)
+				Runes[index]:SetBackdropBorderColor(0, 0, 0)
+				if index == 1 then
+					Runes[index]:SetPoint("TOPRIGHT", Runes)
+				else
+					Runes[index]:SetPoint("RIGHT", Runes[index - 1], "LEFT", -1, 0)
+				end
+			end
+			Runes.colorSpec = true
+			self.Runes = Runes
+		end
 
 		-- TODO: Totems
 		local Totems = {}
@@ -274,8 +313,8 @@ local UnitSpecific = {
 		-- Buffs
 		local Buffs = CreateFrame("Frame", nil, self)
 		Buffs:SetWidth(config.player.width)
-		Buffs:SetHeight(ICON_SIZE * 2)
-		Buffs.size = ICON_SIZE
+		Buffs:SetHeight(config.target.aura.b * 2)
+		Buffs.size = config.target.aura.b
 		Buffs.spacing = 2
 		Buffs["growth-y"] = "DOWN"
 		Buffs.initialAnchor = "TOPLEFT"
@@ -285,8 +324,8 @@ local UnitSpecific = {
 		-- Debuffs
 		local Debuffs = CreateFrame("Frame", nil, self)
 		Debuffs:SetWidth(config.player.width)
-		Debuffs:SetHeight(ICON_SIZE * 2)
-		Debuffs.size = ICON_SIZE
+		Debuffs:SetHeight(config.target.aura.d * 2)
+		Debuffs.size = config.target.aura.d
 		Debuffs.spacing = 2
 		Debuffs["growth-y"] = "UP"
 		Debuffs:SetPoint("BOTTOM", self, "TOP", 0, -1)
@@ -306,12 +345,12 @@ local UnitSpecific = {
 
 		-- Castbar
 		self.CastbarFrame:SetWidth(config.target.width)
-		self.CastbarFrame:SetHeight(ICON_SIZE)
+		self.CastbarFrame:SetHeight(20)
 		self.CastbarFrame:SetPoint("TOPLEFT", self, "BOTTOMLEFT", 0, -3)
 
 		local SpellIcon = self.Castbar:CreateTexture(nil, "OVERLAY")
 		SpellIcon:SetPoint("TOPRIGHT", self.CastbarFrame)
-		SpellIcon:SetSize(ICON_SIZE, ICON_SIZE)
+		SpellIcon:SetSize(20, 20)
 		SpellIcon:SetTexCoord(.06, .94, .06, .94)
 
 		local TimeText = self.Castbar:CreateFontString(nil, "OVERLAY", "TextNormalRight")
@@ -357,10 +396,15 @@ local UnitSpecific = {
 		self.Health:SetPoint("TOPLEFT")
 		self.Health:SetPoint("BOTTOMRIGHT")
 
-		--[[ Power
-		self.Power:SetPoint("BOTTOMLEFT", self, "BOTTOMLEFT", 0, 0)
-		self.Power:SetPoint("RIGHT")
-		self.Power:SetHeight(config.targettarget.power)]]
+		-- Debuffs
+		local Debuffs = CreateFrame("Frame", nil, self)
+		Debuffs:SetWidth(config.focus.width)
+		Debuffs:SetHeight(config.focus.aura.d * 2)
+		Debuffs.size = config.focus.aura.d
+		Debuffs.spacing = 2
+		Debuffs["growth-y"] = "UP"
+		Debuffs:SetPoint("BOTTOM", self, "TOP", 0, -1)
+		Debuffs.PostCreateIcon = PostCreateAura
 
 		-- Health Tags position and settings
 		--self.HealthText:SetPoint("CENTER", self, "CENTER", 0, 0)
@@ -371,7 +415,7 @@ local UnitSpecific = {
 
 		-- Level & Name Tag
 		self.NameText:SetPoint("CENTER", self, "CENTER", 0, 0)
-		self.LevelText:SetPoint("RIGHT", self, "RIGHT", -1, 0)
+		self.LevelText:SetPoint("RIGHT", self, "RIGHT", 0, 0)
 
 		--[[ Castbar
 		self.CastbarFrame:SetSize(config.targettarget.width, 15)
@@ -380,10 +424,8 @@ local UnitSpecific = {
 		local SpellText = self.Castbar:CreateFontString(nil, "OVERLAY", "GameFontNormal")
 		SpellText:SetPoint("LEFT", self.Castbar, 1, 0)]]
 
-		--[[ Register with oUF
-		self:SetWidth(config.targettarget.width)
-		self:SetHeight(config.player.height + config.player.power + 1)
-		self.Castbar.Text = SpellText]]
+		-- Register with oUF
+		self.Debuffs = Debuffs
 	end,
 
 	focus = function(self)
@@ -394,6 +436,16 @@ local UnitSpecific = {
 		-- Health
 		self.Health:SetPoint("TOPLEFT")
 		self.Health:SetPoint("BOTTOMRIGHT")
+
+		-- Debuffs
+		local Debuffs = CreateFrame("Frame", nil, self)
+		Debuffs:SetWidth(config.focus.width)
+		Debuffs:SetHeight(config.focus.aura.d * 2)
+		Debuffs.size = config.focus.aura.d
+		Debuffs.spacing = 2
+		Debuffs["growth-y"] = "UP"
+		Debuffs:SetPoint("BOTTOM", self, "TOP", 0, -1)
+		Debuffs.PostCreateIcon = PostCreateAura
 
 		--[[ Power
 		self.Power:SetPoint("BOTTOMLEFT", self, "BOTTOMLEFT", 0, 0)
@@ -409,7 +461,7 @@ local UnitSpecific = {
 
 		-- Level & Name Tag
 		self.NameText:SetPoint("CENTER", self, "CENTER", 0, 0)
-		self.LevelText:SetPoint("RIGHT", self, "RIGHT", -1, 0)
+		self.LevelText:SetPoint("RIGHT", self, "RIGHT", 0, 0)
 
 		--[[ Castbar
 		self.CastbarFrame:SetSize(config.focus.width, 15)
@@ -418,10 +470,54 @@ local UnitSpecific = {
 		local SpellText = self.Castbar:CreateFontString(nil, "OVERLAY", "GameFontNormal")
 		SpellText:SetPoint("LEFT", self.Castbar, 1, 0)]]
 
-		--[[ Register with oUF
+		-- Register with oUF
+		self.Debuffs = Debuffs
+	end,
+
+	pet = function(self)
+		-- Unit
 		self:SetWidth(config.focus.width)
-		self:SetHeight(config.player.height + config.player.power + 1)
-		self.Castbar.Text = SpellText]]
+		self:SetHeight(config.focus.height)
+
+		-- Health
+		self.Health:SetPoint("TOPLEFT")
+		self.Health:SetPoint("BOTTOMRIGHT")
+
+		-- Debuffs
+		local Debuffs = CreateFrame("Frame", nil, self)
+		Debuffs:SetWidth(config.focus.width)
+		Debuffs:SetHeight(config.focus.aura.d * 2)
+		Debuffs.size = config.focus.aura.d
+		Debuffs.spacing = 2
+		Debuffs["growth-y"] = "UP"
+		Debuffs:SetPoint("BOTTOM", self, "TOP", 0, -1)
+		Debuffs.PostCreateIcon = PostCreateAura
+
+		--[[ Power
+		self.Power:SetPoint("BOTTOMLEFT", self, "BOTTOMLEFT", 0, 0)
+		self.Power:SetPoint("RIGHT")
+		self.Power:SetHeight(config.focus.power)]]
+
+		-- Health Tags position and settings
+		--self.HealthText:SetPoint("CENTER", self, "CENTER", 0, 0)
+		--self.HealthPerText:SetPoint("RIGHT", self.Health.textSeparator, "LEFT", 0, -0)
+
+		-- Power Tags position and settings
+		--self.PowerValue:SetPoint("BOTTOMRIGHT", self.Power, "TOPRIGHT", 0, 1)
+
+		-- Level & Name Tag
+		self.NameText:SetPoint("CENTER", self, "CENTER", 0, 0)
+		self.LevelText:SetPoint("RIGHT", self, "RIGHT", 0, 0)
+
+		--[[ Castbar
+		self.CastbarFrame:SetSize(config.focus.width, 15)
+		self.CastbarFrame:SetPoint("BOTTOMLEFT", self.NameText, "TOPLEFT", 0, 1)
+		self.Castbar:SetAllPoints(self.CastbarFrame)
+		local SpellText = self.Castbar:CreateFontString(nil, "OVERLAY", "GameFontNormal")
+		SpellText:SetPoint("LEFT", self.Castbar, 1, 0)]]
+
+		-- Register with oUF
+		self.Debuffs = Debuffs
 	end,
 
 	party = function(self)
@@ -432,6 +528,25 @@ local UnitSpecific = {
 		local RoleIcon = self.CreateTexture(nil, "OVERLAY")
 		RoleIcon:SetPoint("TOPRIGHT", self, "TOPRIGHT", 0, 0)
 		RoleIcon:SetSize(10, 10)
+	end,
+
+	boss = function(self)
+		-- Unit
+		self:SetWidth(config.boss.width)
+		self:SetHeight(config.boss.height)
+
+		-- Health
+		self.Health:SetPoint("TOPLEFT")
+		self.Health:SetPoint("BOTTOMRIGHT")
+
+		-- Power
+		self.Power:SetWidth(config.boss.width / 2)
+		self.Power:SetHeight(config.boss.power)
+		self.Power:SetPoint("BOTTOMRIGHT", self, "BOTTOMRIGHT", 0, 0)
+
+		-- Name tag & Health
+		self.NameText:SetPoint("LEFT", self, "LEFT", 1, 0)
+		self.HealthText:SetPoint("RIGHT", self, "RIGHT", -1, 0)
 	end
 }
 
@@ -469,26 +584,27 @@ local function Shared(self, unit)
 	Health.colorReaction = false
 	Health.colorHealth = false
 
+	-- StringParent
+	local StringParent = CreateFrame("Frame", "StringParent", self)
+	StringParent:SetFrameStrata("LOW")
+	StringParent:SetFrameLevel(5)
+
 	-- Level Tag
-	local LevelText = self:CreateFontString(nil, "OVERLAY", "TextNormalLeft")
+	local LevelText = StringParent:CreateFontString(nil, "OVERLAY", "TextNormalLeft")
 	LevelText:SetTextColor(1, 1, 1)
 	self.LevelText = LevelText
 	self:Tag(self.LevelText, "([difficulty][level][shortclassification]|r)")
 	
 	-- Name Tag
-	local NameText = self:CreateFontString(nil, "OVERLAY", "TextNormalCenter")
+	local NameText = StringParent:CreateFontString(nil, "OVERLAY", "TextNormalCenter")
 	NameText:SetTextColor(1, 1, 1, 1)
 	self.NameText = NameText
 	self:Tag(self.NameText, "[name]")
 
-	-- StringParent
-	local StringParent = CreateFrame("Frame", "StringParent", self)
-	StringParent:SetFrameStrata("LOW")
-	StringParent:SetFrameLevel(5)
 	-- Health Tag
 	local HealthText = StringParent:CreateFontString(nil, "OVERLAY", "TextNormalCenter")
 	HealthText:SetTextColor(1, 1, 1)
-	local HealthPerText = self:CreateFontString(nil, "OVERLAY", "TextNormalRight")
+	local HealthPerText = StringParent:CreateFontString(nil, "OVERLAY", "TextNormalRight")
 	HealthPerText:SetTextColor(1, 1, 1)
 	self.HealthText = HealthText
 	self.HealthPerText = HealthPerText
@@ -548,12 +664,17 @@ oUF:RegisterStyle("Grumpy", Shared)
 
 oUF:Factory(function(self)
 	--activate style
-	oUF:SetActiveStyle("Grumpy")
+	self:SetActiveStyle("Grumpy")
 
 	--Spawn more OVERLORDS
-	oUF:Spawn("player"):SetPoint("CENTER", UIParent, -260, -230)
-	oUF:Spawn("target"):SetPoint("CENTER", UIParent, 260, -230)
-	oUF:Spawn("targettarget"):SetPoint("CENTER", UIParent, 500, -180)
-	oUF:Spawn("focus"):SetPoint("CENTER", UIParent, -500, -180)
+	self:Spawn("player"):SetPoint("CENTER", UIParent, -260, -230)
+	self:Spawn("pet"):SetPoint("TOPRIGHT", oUF_GrumpyPlayer, "BOTTOMLEFT", -1, -1)
+	self:Spawn("target"):SetPoint("CENTER", UIParent, 260, -230)
+	self:Spawn("targettarget"):SetPoint("CENTER", UIParent, 500, -180)
+	self:Spawn("focus"):SetPoint("CENTER", UIParent, -500, -180)
+
+	for i = 1, 5 do
+		self:Spawn("boss"..i):SetPoint("BOTTOMRIGHT", UIParent, "CENTER", 0, (config.boss.height * i))
+	end
 end)
 
